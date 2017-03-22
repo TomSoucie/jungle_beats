012d17afe84cf8cfddd37e9d5015bb82814dbc73
Thomas Soucie <tsoucie1858@gmail.com>
2017-03-21 21:50:48 -0600

    Commits single append function and testing

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,3 +1,29 @@
+require './lib/node/'
+
 class LinkedList
+  attr_reader   :head
+  def initialize
+    @head = nil
+  end
+
+  def append(name)
+    if @head.nil?
+      @head = Node.new(name)
+    else
+      current = @head
+      while current.next_node != nil
+        current = current.next_node
+      end
+      current.next_node = Node.new(name)
+    end
+  end
+
+  def count
+    1
+  end
+
+  def to_string
+    @head.data
+  end
 
 end
--- test/linked_list_test.rb
+++ test/linked_list_test.rb
@@ -1,8 +1,36 @@
-gem 'minitest' '~> 5.2'
+gem 'minitest', '~> 5.2'
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/linked_list'
 
 class LinkListTest < Minitest::Test
+  def test_the_list_head_is_nil_by_default
+    list = LinkedList.new
 
+    assert_equal nil, list.head
+  end
+
+  def test_the_list_will_append_a_head_with_next_node_nil_by_default
+    list = LinkedList.new
+
+    list.append("doop")
+    
+    assert_equal nil, list.head.next_node
+  end
+
+  def test_the_list_can_return_a_count_of_nodes
+    list = LinkedList.new
+
+    list.append("doop")
+    
+    assert_equal 1, list.count
+  end
+
+  def test_the_list_will_return_a_string_of_data
+    list = LinkedList.new
+
+    list.append("doop")
+
+    assert_equal "doop", list.to_string
+  end
 end
