4b7bcb4ad6f7941775f45192d8420304bdf69a7a
Thomas Soucie <tsoucie1858@gmail.com>
2017-03-22 18:01:53 -0600

    Commits JB append test outline

--- test/jungle_beat_test.rb
+++ test/jungle_beat_test.rb
@@ -14,5 +14,19 @@ class JungleBeatTest < Minitest::Test
     jb = JungleBeat.new
 
     assert_equal LinkedList, jb.list.class
+    assert_equal nil, jb.list.head
+  end
+
+  def test_jungle_beat_can_append_a_string_to_differnt_nodes
+    skip
+    jb = JungleBeat.new
+    
+    jb.append("beep boo la")
+    assert_equal "beep", jb.list.head.data
+    assert_equal "boo", jb.list.next_node.data
+    assert_equal "la", jb.list.next_node.next_node.data
+
+    jb.append("dee beep boo")
+    assert_equal 6, jb.count
   end
 end
